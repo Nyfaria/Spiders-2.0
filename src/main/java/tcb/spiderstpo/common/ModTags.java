37cb532d2ade9e177898bfcf13e6abee075f2afe
Nyfaria DragonMoose <kinaria3324@gmail.com>
2022-10-09 14:14:39 -0500

    full 1.19.2 port

--- src/main/java/tcb/spiderstpo/common/ModTags.java
+++ src/main/java/tcb/spiderstpo/common/ModTags.java
@@ -1,10 +1,9 @@
 package tcb.spiderstpo.common;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.Tag;
 
 public class ModTags {
 	public static final TagKey<Block> NON_CLIMBABLE = BlockTags.create(new ResourceLocation("spiderstpo:non_climbable"));
